36cb57d4b58454073a5e0f0a313df71e8f366e3e
Admin <aidanamamatberdieva2001@gmail.com>
2022-05-22 10:35:29 +0600

    hw17.2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,22 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
 	    Shape circle = new Circle();
-       // circle.pain();
         Shape triangle = new Triangle();
-       // triangle.pain();
         Shape heptagon = new Heptagon();
-       // heptagon.pain();
         Shape pentagon = new Pentagon();
-       // pentagon.pain();
         Shape square = new Square();
-       // square.pain();
-        Shape[]shapes = new Shape[1];
+        Shape[]shapes = {circle, pentagon, heptagon, triangle,square};
         for (Shape shape : shapes) {
-            circle.pain();
-            triangle.pain();
-            pentagon.pain();
-            heptagon.pain();
-            square.pain();
+           shape.pain();
         }
 
     }
